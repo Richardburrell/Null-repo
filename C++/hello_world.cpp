e3cde715671a576cada8c866308e77213ce607a8
Ryan Owens <ryanvader82@gmail.com>
2013-11-15 09:11:40 -0600

    Added test.php and hello world program

---
+++ C++/hello_world.cpp
@@ -0,0 +1,15 @@
+//Including the input/output library
+#include <iostream>
+
+// use a standard C++ namespace
+using namespace std;
+
+// main function
+int main()
+{
+	// cout is like printf in C.  end the line after. 
+    cout << "Hello world!" << endl;
+	// return  0 when completed
+    return 0;
+}
+
