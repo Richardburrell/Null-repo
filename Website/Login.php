af4af9242b1389ce94ef97351db4cd40697b664b
kerrymorris81 <mdellam@siue.edu>
2013-11-18 21:05:51 -0800

    Syntax Cipher Website
    Main Page and Login pages created.

---
+++ Website/Login.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+<title>Untitled 1</title>
+<style type="text/css">
+.loginPageStyle{
+	font-family: "Commodore 64 Angled";
+	background-color: #3A31A6;
+	color: #7C74DF;
+}
+.textCenter {
+	text-align: center;
+}
+.auto-style1 {
+	color: #914141;
+}
+</style>
+</head>
+
+<body class="loginPageStyle">
+ 
+<p class="textCenter">**** COMMODORE 64 BASIC V2 ****</p>
+<p class="loginPageStyle">64K RAM SYSTEM 38911 BASIC BYTES FREE</p>
+<p class="loginPageStyle">READY.</p>
+<p class="loginPageStyle">&nbsp;</p>
+<p class="loginPageStyle">&nbsp;</p>
+<p class="textCenter">WELCOME</p>
+<p class="textCenter">TO</p>
+<p class="textCenter">SYNTAX CIPHERS!</p>
+<p class="textCenter">PLEASE LOGIN.</p>
+<p class="textCenter">&nbsp;</p>
+
+<table width="300" border="0" align="center" outline="0" cellpadding="0" cellspacing="0" >
+<tr>
+<form name="form1" method="post" action="checklogin.php">
+<td>
+<table width="100%" border="0" cellpadding="3" cellspacing="1" >
+<tr>
+<td colspan="3" class="loginPageStyle"><strong>Cipher Login</strong></td>
+</tr>
+<tr>
+<td width="78" class="loginPageStyle">Username</td>
+<td width="6" class="loginPageStyle">:</td>
+<td width="294" class="loginPageStyle"><input name="myusername" type="text" id="myusername"></td>
+</tr>
+<tr>
+<td class="loginPageStyle">Password</td>
+<td class="loginPageStyle">:</td>
+<td class="loginPageStyle"><input name="mypassword" type="text" id="mypassword"></td>
+</tr>
+<tr>
+<td style="height: 36px" align="right" class="loginPageStyle">
+<input type="submit" name="Submit" value="Login" class="loginPageStyle" style="width: 155px"></td>
+</tr>
+</table>
+</td>
+</form>
+</tr>
+</table>
+
+</body>
+
+</html>
