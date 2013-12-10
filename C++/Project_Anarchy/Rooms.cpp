85473f86534a96862f89976055f6f6abbe35852b
jhoff93 <jhoff93@live.com>
2013-12-10 15:53:56 -0600

    Finished the Append Zombie

--- C++/Project_Anarchy/Rooms.cpp
+++ C++/Project_Anarchy/Rooms.cpp
@@ -64,13 +64,15 @@ void roomGraph::setNodeId(const int& nodeiD){
 //----------List Instructions-------
 
 void roomGraph::appendZombie(Zombie& newZombie){     //appends a Zombie node to the end of the list
-    
+    ListTracker* itr = nullptr;
+	for (itr = head, itr != nullptr, itr = itr-> next){
+		itr-> next = new node (newZombie, nullptr);
 }
 
 void roomGraph::deleteZombie(Zombie delZombie){     //this will traverse a list and delete a specific node
     ListTracker* before = head;
 	ListTracker* itr = nullptr;
-	for (itr = head, itr != null, itr = itr-> next){
+	for (itr = head, itr != nullptr, itr = itr-> next){
 		if (head->getEnemy() == delZombie){
 			head = head->next;
 			itr-> next = nullptr;					//this is the special case where the zombie is the first in the list
